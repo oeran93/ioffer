4c50378a7042207643c430e5f4d47b7ccc8c79d3
oeran93 <np.pedretti@gmail.com>
2014-12-06 17:37:33 -0500

    added a new controller, OffersController, with all the methods to handle indexing, showing, creating, editing and deleting an offer. Added a default route to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match ":controller(/:action(/:id))", :via => :get
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
