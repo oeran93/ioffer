a92b9a5e04b760670070f4110efa35f005715c07
Nick Crawford <ncrawford176@gmail.com>
2014-12-11 08:38:31 -0500

    Fixing errors in business model

--- app/models/business.rb
+++ app/models/business.rb
@@ -10,7 +10,7 @@ class Business < ActiveRecord::Base
 	validates :email, :presence => true,
 					  :length => {:maximum => 254},
 					  :format => {:with => /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/ , :message => "The format of your email doesn't look right."},
-					  :uniqueness => {:case_sensitive => false, "Somebody already reagistered with this email. Did you forget your password?"}	
+					  :uniqueness => {:case_sensitive => false, :message => "Somebody already reagistered with this email. Did you forget your password?"}	
 	validates :password, :presence => true,
 					     :length => {:minimum => 8, :message => "Keep your business safe! Your password should be at least 8 characters long. Using numbers and simbols is highly suggested"}
 	validates_presence_of :address
@@ -18,9 +18,9 @@ class Business < ActiveRecord::Base
 	validates_presence_of :state
 	validates_presence_of :zip
 	validates_presence_of :country
-	validates :latitude :presence => true,
+	validates :latitude, :presence => true,
 						:numericality => {:message => "We could not find the specified address. Did you spell everything correctly?"}
-	validates :longitude :presence => true,
+	validates :longitude, :presence => true,
 						:numericality => {:message => "We could not find the specified address. Did you spell everything correctly?"}
 	
 	scope :filter_by_location, lambda {|latMax,latMin,lngMax,lngMin| 
