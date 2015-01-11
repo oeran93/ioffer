c187cd19adf7a872537dbbcceb1aded3a1286609
oeran93 <np.pedretti@gmail.com>
2015-01-11 08:50:52 -0500

    adding filters to business controller. This prevents the user to access pages he is not supposed to access

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,13 +1,13 @@
 class BusinessController < ApplicationController
 
+	before_filter :require_log_in, :only => [:profile, :sign_out] 
+	before_filter :require_not_log_in, :only => [:sign_up,:sign_in]
+	before_filter :require_parameters, :only => [:create, :update, :sign_in_attempt]
+
 	def index
 		#must include an offset and a limit that can be changed 
 		#with show more. 
-		@businesses = Business.all 
-	end
-
-	def sign_in
-		
+		@businesses = Business.all
 	end
 
 	def sign_out
@@ -15,12 +15,15 @@ class BusinessController < ApplicationController
 		redirect_to(:action => "sign_in")
 	end
 
+	def sign_in
+	end
+
 	def sign_in_attempt
 		if (business = Business.find_by_email(params[:business][:email]))
 			if business.authenticate(params[:business][:password])
 				flash[:notice] = "Successfully logged in"
 				session[:id] = business.id
-				redirect_to(:action => "profile", :id => session[:id]) and return
+				redirect_to(:action => "profile") and return
 			end
 		end
 		flash[:notice] = "Wrong email or password"
@@ -32,13 +35,10 @@ class BusinessController < ApplicationController
 	end
 
 	def create
-		if params.blank?
-			redirect_to(:action=>"sign_up")
-		end
 		@business = Business.new(business_params)
-			if !@business.save
-				render("sign_up")
-			end
+		if !@business.save
+			render("sign_up")
+		end
 	end
 
 	def profile
@@ -47,8 +47,6 @@ class BusinessController < ApplicationController
 
 	def update
 		@business = Business.find(params[:id])
-		params[:business][:latitude]= 3
-		params[:business][:longitude]= 4
 		if @business.update_attributes(business_params)
 			redirect_to(:action => 'profile', :id => @business.id) and return
 		end
@@ -56,9 +54,28 @@ class BusinessController < ApplicationController
 	end
 
 	private
+		
 		def business_params
 				params.require(:business).permit(:name, :email, :password, :latitude, 
 					:longitude, :country, :city, :state, :zip, :address, :image, :phone, :website, 
 					:new_password, :new_password_confirmation, :old_password)
+		end
+
+		def require_log_in
+			if !view_context.is_user_signed_in
+				redirect_to(:action => "sign_in")
+			end
+		end
+
+		def require_not_log_in
+			if view_context.is_user_signed_in
+				redirect_to(:action => "profile")
+			end
+		end
+
+		def require_parameters
+			if params.blank?
+				redirect_to(:action => "sign_in")
+			end
 		end
 end
