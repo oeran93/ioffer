3273d453a0eabe3090b8f469bf9e249cdbc8a722
Nick Crawford <ncrawford176@gmail.com>
2015-01-16 14:43:02 -0500

    Fixing layout for index and profile button

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20150115023607) do
     t.string   "name",               limit: 50,  null: false
     t.float    "latitude",           limit: 24,  null: false
     t.float    "longitude",          limit: 24,  null: false
+    t.string   "address"
     t.string   "website"
     t.string   "phone"
-    t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "country"
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 20150115023607) do
   end
 
   add_index "businesses_tags", ["business_id", "tag_id"], name: "index_businesses_tags_on_business_id_and_tag_id", using: :btree
+
+  create_table "offer_dates", force: true do |t|
+    t.integer  "offer_id"
+    t.date     "date"
+    t.time     "time_start"
+    t.time     "time_end"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "offers", force: true do |t|
     t.integer  "business_id"
@@ -70,7 +79,7 @@ ActiveRecord::Schema.define(version: 20150115023607) do
   create_table "user_offer_opinions", force: true do |t|
     t.integer  "offer_id"
     t.integer  "user_id"
-    t.integer  "opinion"
+    t.integer  "opinion",    limit: 1
     t.datetime "created_at"
     t.datetime "updated_at"
   end
