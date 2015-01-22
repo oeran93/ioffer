a1a8a568135e9a709d38163721950fb7dc35729d
oeran93 <np.pedretti@gmail.com>
2015-01-21 20:18:21 -0500

    changing the offer_controller not to allow wrong offers ( that end before they start). returning a flash notice to the manage page alerting the user of the success or failur of the offer creation/deletion

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,7 +7,7 @@ class OffersController < ApplicationController
 
   before_filter :require_log_in, :only => [:manage,:create,:delete]
   before_filter :require_parameters, :only => [:delete, :create]
-  before_filter :require_offer_ownership, :only => [:delete]
+  #before_filter :require_offer_ownership, :only => [:delete]
 
   def index
     #(param(:latitude),param(:longitude),param(:miles_range))
@@ -28,21 +28,23 @@ class OffersController < ApplicationController
 
   def create
     @offer = Offer.new(offer_params)
-    if @offer.save
+    if set_time_attributes_from_params
       @business = Business.find(session[:id])
       @business.offers << @offer
       @offer_date = OfferDate.new(offer_date_params)
       @offer.offer_dates << @offer_date
       flash[:notice]="Offer created successfully"
     else
-      flash[:notice]="An error occurred while activating the offer"
+      flash[:notice]="Your offer ends before it starts!"
     end
+    redirect_to(:action => "manage") 
   end
 
   def delete
-    @offer = Offer.find(params[:offer][:id])
+    @offer = Offer.find(params[:offer][:offer_id])
     @offer.destroy
     flash[:notice] = "Offer successfully deleted"
+    redirect_to(:action => "manage")
   end
 
   private
@@ -51,23 +53,19 @@ class OffersController < ApplicationController
       params.require(:offer).permit(:percentage)
     end
 
-    def offer_date_params
-      set_time_attributes_from_params 
+    def offer_date_params 
       params.require(:offer).permit(:start_time,:end_time,:visible_from)
     end
 
     def set_time_attributes_from_params
-      year = params[:offer][:year].to_i
-      month = params[:offer][:month].to_i
-      day = params[:offer][:day].to_i
-      start_hour = params[:offer]['time_start(4i)'].to_i
-      start_minute = params[:offer]['time_start(5i)'].to_i
-      end_hour = params[:offer]['time_end(4i)'].to_i
-      end_minute = params[:offer]['time_end(5i)'].to_i
+      year, month, day = params[:offer][:year].to_i, params[:offer][:month].to_i, params[:offer][:day].to_i
+      start_hour, start_minute = params[:offer]['time_start(4i)'].to_i, params[:offer]['time_start(5i)'].to_i
+      end_hour, end_minute = params[:offer]['time_end(4i)'].to_i, params[:offer]['time_end(5i)'].to_i
       visible_hour = start_hour - params[:offer][:visible_from].to_i
       params[:offer][:start_time] = Time.new(year,month,day,start_hour,start_minute).to_i
       params[:offer][:end_time] = Time.new(year,month,day,end_hour,end_minute).to_i
       params[:offer][:visible_from] = Time.new(year,month,day,visible_hour,start_minute).to_i
+      return params[:offer][:end_time] >= params[:offer][:start_time]
     end
 
     def require_log_in 
@@ -85,7 +83,7 @@ class OffersController < ApplicationController
     def require_offer_ownership
       business = Business.find(session[:id])
       business.offers.each do |offer|
-        if offer.id == params[:offer_id]
+        if offer.id == params[:offer][:offer_id]
           return
         end
       end
