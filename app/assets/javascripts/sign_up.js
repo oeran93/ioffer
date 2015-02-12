6fb64220b5192121dca2117398fd12ff634ef93b
Nick Crawford <ncrawford176@gmail.com>
2015-02-11 21:39:42 -0500

    Styling user sign in/ sign up page. Fix buttons in sign up

--- app/assets/javascripts/sign_up.js
+++ app/assets/javascripts/sign_up.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
 	
-	$(".submit_btn").click(function(){
+	$(".submit_btn:not(#signup_finish)").click(function(){
 		$(".tab_nav").hide()
 		$(".tab").hide()
-		$($(this).data("nav_id")).show()
-		$($(this).data("tab_id")).show()
+		$($(this).data("nav-id")).show()
+		$($(this).data("tab-id")).show()
+	});
+
+	$(".signup_next").click(function(){
+		var active_tab_bar = $("ul li[class=active]")
+		active_tab_bar.next().addClass("active")
+	});
+
+	$(".signup_back").click(function(){
+		var active_tab_bar = $("ul li[class=active]")
+		active_tab_bar.last().removeClass("active")
 	});
 
 });
