95af8a6ef026b55545d0984401dd9557940fc154
oeran93 <np.pedretti@gmail.com>
2015-04-17 10:53:53 -0400

    adding server initial localization

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -10,8 +10,8 @@ class OffersController < ApplicationController
   before_filter :require_offer_ownership, :only => [:delete]
 
   def index
-    @latitude = request.location.latitude.to_s
-    @longitude = request.location.longitude.to_s
+    @latitude = request.location.latitude.to_s || "45.471041"
+    @longitude = request.location.longitude.to_s || "9.183054"
     search = SearchHelper.new([])
     search.filter_by_location(@latitude.to_s+', '+@longitude.to_s)
     @offers = search.get_offers
